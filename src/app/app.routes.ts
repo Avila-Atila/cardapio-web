bd66c299ff3053c04c9c28f96b2e64fb299d0873
Atila Garcia Avila <atilagarcia12@gmail.com>
2025-05-12 16:42:07 -0300

    comit inicial

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    pathMatch: 'full',
+    loadComponent() {
+      return import('./pages/home/home.component').then((x) => x.HomeComponent);
+    },
+  },
+  {
+    path: '**',
+    pathMatch: 'full',
+    redirectTo: '',
+  },
+];
